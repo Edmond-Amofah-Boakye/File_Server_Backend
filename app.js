0217670eb7ae492d5e04387df1345ef0f4a97591
Edmond Amofah Boakye <edmondboakye1622@gmail.com>
2023-07-02 21:55:11 +0000

    updated cors

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/api', limit)
 
 //using cors middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://fileserverapp.onrender.com",
     credentials: true
 }))
 
