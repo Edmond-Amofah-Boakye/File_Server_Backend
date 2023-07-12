1eb17e644f013a9a0ec3b41b1258f9935fcf0fc7
Edmond-Amofah-Boakye <80597593+Edmond-Amofah-Boakye@users.noreply.github.com>
2023-07-12 23:17:10 +0000

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,3 +1,8 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import AppError from '../utils/AppError.js'
 import multer from 'multer';
 
